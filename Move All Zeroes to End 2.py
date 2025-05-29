c7c540cb9b9bd61ff1ddbcf18c209947f6df3369
Prince John <125338190+princejohnongit@users.noreply.github.com>
2025-05-29 18:35:21 +0530

    Update Move All Zeroes to End 2.py

--- Move All Zeroes to End 2.py
+++ Move All Zeroes to End 2.py
@@ -3,6 +3,9 @@
 class Solution:
     def pushZerosToEnd(self,arr):
         #zero_position_pointer: zpp
+        #zpp is where the zero is currently assumed to be, a trash position to switch\
+        #when you see a non-zero element swap it with zpp, make the next position to 
+        #be the trash pointer aka zpp
         zpp=0
         for i in range(len(arr)):
             if arr[i]!=0:
